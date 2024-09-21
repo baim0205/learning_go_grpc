a5cb8e3160ae98f71ac113a0ac8d4ce80a9feda4
Mohammad Nurrohim <mohammad.nurrohim@aladinbank.id>
2024-09-21 21:58:42 +0700

    create database and function read, update, delete

---
+++ auth-service/database/db.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	dsn := "root:root@tcp(127.0.0.1:5555)/auth_service"
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("Failed to connect to DB: %v", err)
+	}
+	DB = db
+
+	// Test the connection
+	err = DB.Ping()
+	if err != nil {
+		log.Fatalf("Failed to ping DB: %v", err)
+	}
+	log.Println("Connected to MySQL Database!")
+}
---
+++ database/example/insert-item.sql
@@ -0,0 +1,2 @@
+INSERT INTO items (name, description) 
+VALUES ('Item 1', 'Description for item 1'), ('Item 2', 'Description for item 2');
